40cc4ebc401473d3346775ae37bf39478b84a6a4
lkjy <2748517089@qq.com>
2016-01-08 18:56:24 +0800

    我是王继红

---
+++ qweawtry/qweawtry/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  qweawtry
+//
+//  Created by lkjy on 16/1/8.
+//  Copyright © 2016年 信管. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
